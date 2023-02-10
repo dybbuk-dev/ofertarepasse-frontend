53d2976fbfe8b5c91d17790b93feb6a167da37da
Wentony Silva Minutti <wentonyminutti@gmail.com>
2023-02-10 19:28:26 -0300

    Create Sold screen in Dashboard

---
+++ src/pages/Dashboard/Sold/index.tsx
@@ -0,0 +1,12 @@
+import SoldComponent from 'components/organisms/Dashboard/Sold'
+import DashboardTemplate from 'components/templates/DashboardTemplate'
+
+const Sold = () => {
+    return (
+        <DashboardTemplate>
+            <SoldComponent />
+        </DashboardTemplate>
+    )
+}
+
+export default Sold
